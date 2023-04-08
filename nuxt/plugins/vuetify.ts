4ccd9f1bafcba6782a7fdf59c1627820ad8b1892
fujitaiori <ioriori0413@gmail.com>
2023-04-08 13:45:30 +0900

    feat: setup Nuxt3

---
+++ nuxt/plugins/vuetify.ts
@@ -0,0 +1,30 @@
+import { createVuetify } from 'vuetify'
+import { MAIN_THEME, mainTheme, MAIN_DARK_THEME, mainDarkTheme } from '@/helpers/themes'
+import { defaults } from '@/helpers/defaults'
+// import '@mdi/font/css/materialdesignicons.css' // 使用するアイコンを読み込む `mdi-xxx`
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    defaults,
+    display: {
+      mobileBreakpoint: 'sm',
+    },
+    // add theme
+    theme: {
+      defaultTheme: MAIN_THEME,
+      themes: {
+        mainTheme,
+        mainDarkTheme,
+      },
+      // primary-darken-9 primary-lighten-9 までできるようにする
+      variations: {
+        colors: ['primary', 'secondary', 'accent'],
+        lighten: 9,
+        darken: 9,
+      },
+    },
+  })
+
+  nuxtApp.vueApp.use(vuetify)
+})
