a2e1b8acddb32501d21b97b178dbb9b550f813bc
fujitaiori <ioriori0413@gmail.com>
2023-04-24 07:10:58 +0900

    feat: todoのページを分ける

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Welcome to the Index Page</h1>
+    <!-- Your content goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      // Your data properties go here
+    }
+  },
+  methods: {
+    // Your methods go here
+  },
+  computed: {
+    // Your computed properties go here
+  },
+}
+</script>
+
+<style scoped>
+/* Your styles go here */
+</style>
