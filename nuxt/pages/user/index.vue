3367ac69946e86b3eb934ecfe90a78ffa6a874e0
fujitaiori <ioriori0413@gmail.com>
2023-06-03 18:01:12 +0900

    chore: subscribe除去

--- nuxt/pages/user/index.vue
+++ nuxt/pages/user/index.vue
@@ -6,18 +6,18 @@
     <v-btn @click="create">Create</v-btn>
 
     <h2>users</h2>
-    <ul>
+    <ul ref="userList">
       <li v-for="user in users" :key="user.id">
         {{ user.id }} : {{ user.name }}
       </li>
     </ul>
 
-    <h2>asyncUsers</h2>
+    <!-- <h2>asyncUsers</h2>
     <ul>
       <li v-for="user in asyncUsers" :key="user.id">
         {{ user.id }} : {{ user.name }}
       </li>
-    </ul>
+    </ul> -->
     <child-component />
   </div>
 </template>
@@ -40,17 +40,17 @@ export default defineNuxtComponent({
       users: [],
     }
   },
-  async asyncData() {
-    console.log(
-      'asyncData: インスタンス生成前 CSRの場合はコンソールに表示される'
-    )
-    const users = await API.graphql({
-      query: listUsers,
-    })
-    return {
-      asyncUsers: users.data.listUsers.items,
-    }
-  },
+  // async asyncData() {
+  //   console.log(
+  //     'asyncData: インスタンス生成前 CSRの場合はコンソールに表示される'
+  //   )
+  //   const users = await API.graphql({
+  //     query: listUsers,
+  //   })
+  //   return {
+  //     asyncUsers: users.data.listUsers.items,
+  //   }
+  // },
   async beforeCreate() {
     console.log('#### beforeCreate: インスタンス生成前')
     console.log('hoge: ' + this.hoge)
@@ -60,7 +60,6 @@ export default defineNuxtComponent({
     console.log('hoge: ' + this.hoge)
     console.log('マウントしているDOM要素:', this.$el)
     await this.getUsers()
-    this.subscribe()
   },
   beforeMount() {
     console.log('#### beforeMount: マウント直前')
@@ -70,6 +69,10 @@ export default defineNuxtComponent({
     console.log('#### mounted: DOM生成直後に実行')
     console.log('マウントしているDOM要素:', this.$el)
   },
+  beforeUpdate() {
+    console.log('#### beforeUpdate: DOMのrerender前に実行')
+    console.log(`更新前のuserList:`, this.$refs.userList)
+  },
   methods: {
     async create() {
       const user = { name: this.name }
@@ -77,7 +80,6 @@ export default defineNuxtComponent({
         query: createUser,
         variables: { input: user },
       })
-      this.name = ''
     },
     async getUsers() {
       const users = await API.graphql({
